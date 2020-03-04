fde9ddfeb659c65c4cb9bcc67385c2ead2f5012b
alonzno <alonzoa@cs.uoregon.edu>
2020-03-04 15:32:14 -0800

    Remove credentials, add get_token function

--- spotify_lyrics.py
+++ spotify_lyrics.py
@@ -1,55 +1,18 @@
-#!/usr/local/bin/python3
-
 import sys
 import re
 import os
-import urllib.request
 import json
+import time
+import urllib.request
 
 import spotipy
 import spotipy.util as util
 from spotipy.oauth2 import SpotifyClientCredentials
 from bs4 import BeautifulSoup
 
-# TODO: REMOVE THESE LINES, BIG SECURITY VULNERABILITY
-os.environ["SPOTIPY_CLIENT_ID"] = "e43b96ffdef54e2a8f6aab6b0baa5483"
-os.environ["SPOTIPY_CLIENT_SECRET"] = "61c58e9e13174cd58a7f27e7d50e4f4c"
-
 '''
-birdy_uri = 'spotify:artist:2WX2uTcsvV5OnS0inACecP'
-spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
-
-results = spotify.artist_albums(birdy_uri, album_type='album')
-albums = results['items']
-while results['next']:
-    results = spotify.next(results)
-    albums.extend(results['items'])
-
-for album in albums:
-    print(album['name'])
-print("\n\n\n\n")
+	BEGIN Function Declarations
 '''
-
-
-username = "alonzoa-us"
-client_id = "e43b96ffdef54e2a8f6aab6b0baa5483"
-client_secret = "61c58e9e13174cd58a7f27e7d50e4f4c"
-redirect_uri = "http://127.0.0.1/callback"
-
-
-scope = 'user-read-currently-playing'
-# scope = 'user-read-playback-state'
-# works as well
-
-token = util.prompt_for_user_token(username, scope, client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri)
-
-spotify = spotipy.Spotify(auth=token)
-current_track_dict = spotify.current_user_playing_track()
-
-current_track_name = current_track_dict['item']['name']
-current_artist = current_track_dict['item']['artists'][0]['name']
-
-
 def get_lyrics(artist,song_title):
     artist = artist.lower()
     song_title = song_title.lower()
@@ -72,6 +35,57 @@ def get_lyrics(artist,song_title):
         lyrics = lyrics.replace('<br>','').replace('</br>','').replace('<br/>', '').replace('</div>','').strip()
         return lyrics
     except Exception as e:
-        return "Exception occurred \n" +str(e)
+        return "Woops, can't find that one.\n" +str(e)
 
+def get_token():
+	try:
+		cred_f = open('credentials.json')
+		credentials = json.loads(cred_f.read())
+		username = credentials['username']
+		client_id = credentials['id']
+		client_secret = credentials['secret']
+		redirect_uri = credentials['redirect']
+		scope = 'user-read-currently-playing'
+
+		try:
+			cache_f = open('.cache-alonzoa-us')
+			data = json.loads(cache_f.read())
+			if (data['expires_at'] > int(time.time())):
+				return util.prompt_for_user_token(username, scope, client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri)
+			else:
+				raise IOError
+		except IOError:
+			#TODO ADD server intercept req
+			return util.prompt_for_user_token(username, scope, client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri)
+		finally:
+			cache_f.close()
+	except IOError:
+		print('It appears you are lacking the credentials.json file.')
+		print('No soup for you')
+	finally:
+		cred_f.close()
+
+
+'''
+	END Fundtion Declarations
+'''
+
+'''
+	BEGIN Main Method
+'''
+
+token = get_token()
+
+spotify = spotipy.Spotify(auth=token)
+current_track_dict = spotify.current_user_playing_track()
+current_track_name = current_track_dict['item']['name']
+current_artist = current_track_dict['item']['artists'][0]['name']
+
+header = current_track_name + ' by ' + current_artist
+print('-'*len(header))
+print(header)
+print('-'*len(header)+'\n')
 print(get_lyrics(current_artist, current_track_name))
+'''
+	END Main Method
+'''
