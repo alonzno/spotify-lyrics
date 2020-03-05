1375a3710908c2a388e349151e68d7c68d939aec
alonzno <alonzoa@cs.uoregon.edu>
2020-03-04 19:11:58 -0800

    Add Automatic OAuth authentication

--- spotify_lyrics.py
+++ spotify_lyrics.py
@@ -4,6 +4,7 @@ import os
 import json
 import time
 import urllib.request
+from http.server import HTTPServer, BaseHTTPRequestHandler
 
 import spotipy
 import spotipy.util as util
@@ -11,8 +12,101 @@ from spotipy.oauth2 import SpotifyClientCredentials
 from bs4 import BeautifulSoup
 
 '''
+	BEGIN Class Declarations
+'''
+class OAuthRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        request_path = self.path
+        self.server.url = request_path
+
+class OAuthHTTPServer:
+    def __init__(self, addr):
+        self.server = HTTPServer(addr, OAuthRequestHandler)
+        self.server.url = None
+
+'''	
+	END Class Declarations
+'''
+
+'''
 	BEGIN Function Declarations
 '''
+def prompt_for_user_token_mod(
+    username,
+    scope=None,
+    client_id=None,
+    client_secret=None,
+    redirect_uri=None,
+    cache_path=None,
+    oauth_manager=None,
+    show_dialog=False
+):
+    if not oauth_manager:
+        if not client_id:
+            client_id = os.getenv("SPOTIPY_CLIENT_ID")
+
+        if not client_secret:
+            client_secret = os.getenv("SPOTIPY_CLIENT_SECRET")
+
+        if not redirect_uri:
+            redirect_uri = os.getenv("SPOTIPY_REDIRECT_URI")
+
+        if not client_id:
+            print(
+                """
+                You need to set your Spotify API credentials.
+                You can do this by setting environment variables like so:
+                export SPOTIPY_CLIENT_ID='your-spotify-client-id'
+                export SPOTIPY_CLIENT_SECRET='your-spotify-client-secret'
+                export SPOTIPY_REDIRECT_URI='your-app-redirect-url'
+                Get your credentials at
+                    https://developer.spotify.com/my-applications
+            """
+            )
+            raise spotipy.SpotifyException(550, -1, "no credentials set")
+
+        cache_path = cache_path or ".cache-" + username
+
+    sp_oauth = oauth_manager or spotipy.SpotifyOAuth(
+        client_id,
+        client_secret,
+        redirect_uri,
+        scope=scope,
+        cache_path=cache_path,
+        show_dialog=show_dialog
+    )
+
+    # try to get a valid token for this user, from the cache,
+    # if not in the cache, the create a new (this will send
+    # the user to a web page where they can authorize this app)
+
+    token_info = sp_oauth.get_cached_token()
+
+    if not token_info:
+
+        server_address = ('', 8420)
+        httpd = OAuthHTTPServer(server_address)
+
+        import webbrowser
+
+        webbrowser.open(sp_oauth.get_authorize_url())
+        httpd.server.handle_request()
+
+
+        if httpd.server.url:
+            url = httpd.server.url
+
+            code = sp_oauth.parse_response_code(url)
+            token = sp_oauth.get_access_token(code, as_dict=False)
+    else:
+        return token_info["access_token"]
+
+    # Auth'ed API request
+    if token:
+        return token
+    else:
+        return None
+
 def get_lyrics(artist,song_title):
     artist = artist.lower()
     song_title = song_title.lower()
@@ -38,6 +132,9 @@ def get_lyrics(artist,song_title):
         return "Woops, can't find that one.\n" +str(e)
 
 def get_token():
+	cred_f = None
+	cache_f = None
+
 	try:
 		cred_f = open('credentials.json')
 		credentials = json.loads(cred_f.read())
@@ -56,14 +153,17 @@ def get_token():
 				raise IOError
 		except IOError:
 			#TODO ADD server intercept req
-			return util.prompt_for_user_token(username, scope, client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri)
+			return prompt_for_user_token_mod(username, scope, client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri)
 		finally:
-			cache_f.close()
+			if cache_f:
+				cache_f.close()
 	except IOError:
 		print('It appears you are lacking the credentials.json file.')
 		print('No soup for you')
+		quit()
 	finally:
-		cred_f.close()
+		if cred_f:
+			cred_f.close()
 
 
 '''
@@ -73,6 +173,8 @@ def get_token():
 '''
 	BEGIN Main Method
 '''
+
+#TODO add args
 
 token = get_token()
 
